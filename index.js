836ff3e96951de657581d3bbdcb3da6c9b64bc69
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-22 20:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function currentLine(line) {
   if(line.length === 0) {
     return `The line is currently empty.`
   }
-    for(var i = 0; i < line.length; i++) {        //the length is 3, but the index starts at zero. Remember the +1 nature of index #'s
+    else {
+      for(var i = 0; i < line.length; i++) {        //the length is 3, but the index starts at zero. Remember the +1 nature of index #'s
       lineOfPeople.push(` ${i+1}. ${line[i]}`);  //i+1 makes the number in line accurate, so line[0] will be written as 1. John, 2. Joe, etc.
     }
     return `The line is currently:${lineOfPeople}`;
