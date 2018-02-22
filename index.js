48f33a37d0b2c933316be67500ab10bfaa66749a
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-22 20:34:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
+function currentLine(line) {
+  var lineOfPeople = [];
+  if(line.length === 0) {
+    return `The line is currently empty.`
+  }
+    for(var i = 0; i < line.length; i++) {        //the length is 3, but the index starts at zero. Remember the +1 nature of index #'s
+      lineOfPeople.push(` ${i+1}. ${line[i]}`);  //i+1 makes the number in line accurate, so line[0] will be written as 1. John, 2. Joe, etc.
+    }
+    return `The line is currently:${lineOfPeople}`;
+  }
 
-var katzDeli = [];
-
-function takeANumber(line, name) {
-line.push(name);
-console.log ('Welcome, ' + name + '. You are number ' + line.length + ' in line.');
-return 'Welcome, ' + name + '. You are number ' + line.length + ' in line.';
+function takeANumber(line,name) {
+  line.push(name);                                                   //push puts the parameter name at the end of parameter line
+  return `Welcome, ${name}. You are number ${line.length} in line.` //simple interpolation ${}
 }
 
 function nowServing(line) {
-if(line.length === 0) {
-  console.log ("There is nobody waiting to be served!");
-  return "There is nobody waiting to be served!";
-}
-else {
-  return `Currently serving ${line.shift}.`;
-}
-}
-
-function currentLine(line) {
-if(!line.length) {
-  return "The line is currently empty.";
-}
-var peopleinline= [];
-for (var i=0; i<line.length; i++) {
-  peopleinline.push(i+1 + ". " + line[i]);
-}
-console.log('The line is currently: ' + peopleinline);
-return "The line is currently: " + peopleinline.join(', ');
+  if(line.length === 0) {
+    return `There is nobody waiting to be served!`
+  }
+  else if(line.length > 0) {
+    return `Currently serving ${line.shift()}.`  //takes item from beginning of array via shift
+  }
 }
